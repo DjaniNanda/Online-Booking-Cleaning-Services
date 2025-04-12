81cd57e440d4a11a744cbda157a4dc55fadb75e3
DjaniNanda <djaninandapetrus@gmail.com>
2025-04-12 05:34:12 +0200

    Update build

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -o errexit
-pip install -r ./requirements.txt
+
+# Change to the backend directory
+cd "$(dirname "$0")"
+
+# Now we can find requirements.txt in the current directory
+pip install -r requirements.txt
+
+# Run Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
