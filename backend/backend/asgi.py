0162c01cf1208b5224be3bf34c5b739d5b392996
DjaniNanda <djaninandapetrus@gmail.com>
2025-04-12 15:50:07 +0200

    Update asgi.py

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -6,12 +6,17 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
-
 import os
+import sys
+from pathlib import Path
+
+# Add the project root to the Python path
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(BASE_DIR))
 
 from django.core.asgi import get_asgi_application
 
-settings_module = 'backend.backend.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME'in os.environ else 'backend.backend.settings'
+settings_module = 'backend.backend.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'backend.backend.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_asgi_application()
